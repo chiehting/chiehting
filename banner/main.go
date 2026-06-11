85f102d0fa555f2896de4da2949faf587130aac2
chiehting <ting911111@gmail.com>
2026-06-11 20:48:36 +0800

    test

--- banner/main.go
+++ banner/main.go
@@ -1,22 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"image"
 	"image/color"
 	"image/gif"
-	"io/ioutil"
 	"math"
 	"os"
 
 	"github.com/golang/freetype"
 )
 
-// Circle position
 type Circle struct {
 	X, Y, R float64
 }
 
-// Brightness is return
 func (c *Circle) Brightness(x, y float64) uint8 {
 	var dx, dy float64 = c.X - x, c.Y - y
 	d := math.Sqrt(dx*dx+dy*dy) / c.R
@@ -30,46 +28,66 @@ func main() {
 	var w, h int = 840, 90
 	circles := make(map[int]*Circle)
 	var palette = []color.Color{
-		color.RGBA{0x00, 0x00, 0x00, 0xff},
-		color.RGBA{0x00, 0x00, 0xff, 0xff},
-		color.RGBA{0x00, 0xff, 0x00, 0xff},
-		color.RGBA{0x00, 0xff, 0xff, 0xff},
-		color.RGBA{0xff, 0x00, 0x00, 0xff},
-		color.RGBA{0xff, 0x00, 0xff, 0xff},
-		color.RGBA{0xff, 0xff, 0x00, 0xff},
-		color.RGBA{0xff, 0xff, 0xff, 0xff},
+		color.RGBA{0x00, 0x00, 0x00, 0xff}, // 黑
+		color.RGBA{0x00, 0x00, 0xff, 0xff}, // 藍
+		color.RGBA{0x00, 0xff, 0x00, 0xff}, // 綠
+		color.RGBA{0x00, 0xff, 0xff, 0xff}, // 青
+		color.RGBA{0xff, 0x00, 0x00, 0xff}, // 紅
+		color.RGBA{0xff, 0x00, 0xff, 0xff}, // 品紅
+		color.RGBA{0xff, 0xff, 0x00, 0xff}, // 黃
+		color.RGBA{0xff, 0xff, 0xff, 0xff}, // 白
+	}
+
+	fontPath := "/usr/share/fonts/truetype/ubuntu/UbuntuSansMono-Italic[wght].ttf"
+	fontBytes, err := os.ReadFile(fontPath)
+	if err != nil {
+		fmt.Printf(fontPath, err)
+		return
+	}
+	font, err := freetype.ParseFont(fontBytes)
+	if err != nil {
+		fmt.Printf("%v\n", err)
+		return
 	}
 
 	var images []*image.Paletted
 	var delays []int
-	steps := 70
+	steps := 240
 
 	for step := 0; step <= steps; step++ {
-		img := image.NewPaletted(image.Rect(0, 0, w, h), palette)
-		images = append(images, img)
-		delays = append(delays, 0)
+		rgbaImg := image.NewRGBA(image.Rect(0, 0, w, h))
+		delays = append(delays, 4)
+
+		currentX := float64(w) / float64(steps) * float64(step)
 
 		for i := 0; i < 3; i++ {
 			circles[i] = &Circle{
-				X: float64(w / steps * step),
-				Y: math.Sin(float64(w/steps*step*(i+1))*math.Pi/180)*float64((h-4)/2) + float64(h/2),
+				X: currentX,
+				Y: math.Sin(currentX*float64(i+1)*math.Pi/180)*float64((h-4)/2) + float64(h/2),
 				R: float64(5 * (i + 1)),
 			}
 		}
 
-		for x := 0; x <= w; x++ {
+		for x := 0; x < w; x++ {
 			for y := 0; y < h; y++ {
-				img.Set(x, y, color.RGBA{
-					circles[0].Brightness(float64(x), float64(y)),
-					circles[1].Brightness(float64(x), float64(y)),
-					circles[2].Brightness(float64(x), float64(y)),
-					255,
-				})
+				r := circles[0].Brightness(float64(x), float64(y))
+				g := circles[1].Brightness(float64(x), float64(y))
+				b := circles[2].Brightness(float64(x), float64(y))
+
+				if r > 0 || g > 0 || b > 0 {
+					outR, outG, outB := r, g, b
+					rgbaImg.Set(x, y, color.RGBA{outR, outG, outB, 255})
+
+				} else if y == h/2 {
+					rgbaImg.Set(x, y, color.RGBA{255, 255, 255, 255})
+				} else {
+					rgbaImg.Set(x, y, color.RGBA{0, 0, 0, 255})
+				}
 			}
 
 			for i := 1; i <= 3; i++ {
 				y := math.Sin(float64(x*i)*math.Pi/180) * float64((h-4)/2)
-				img.Set(x, int(y)+(h/2), color.RGBA{
+				rgbaImg.Set(x, int(y)+(h/2), color.RGBA{
 					uint8((i + 1) % 3 * 255),
 					uint8((i + 2) % 3 * 255),
 					uint8((i + 3) % 3 * 255),
@@ -78,24 +96,40 @@ func main() {
 			}
 		}
 
-		fontBytes, _ := ioutil.ReadFile("/System/Library/Fonts/Supplemental/Chalkduster.ttf")
-		font, _ := freetype.ParseFont(fontBytes)
 		c := freetype.NewContext()
-		c.SetDPI(float64(96))
+		c.SetDPI(96)
 		c.SetFont(font)
-		c.SetFontSize(34)
-		c.SetClip(img.Bounds())
-		c.SetDst(img)
+		c.SetFontSize(50)
+		c.SetClip(rgbaImg.Bounds())
+		c.SetDst(rgbaImg)
 		c.SetSrc(image.NewUniform(color.RGBA{255, 255, 255, 255}))
-		//draw the label on image
-		c.DrawString("Hello", freetype.Pt(270, 65))
-		c.DrawString("World", freetype.Pt(430, 65))
+
+		_, _ = c.DrawString("H e l l o", freetype.Pt(50, 65))
+		_, _ = c.DrawString("W o r l d", freetype.Pt(460, 65))
+
+		palettedImg := image.NewPaletted(image.Rect(0, 0, w, h), palette)
+		for x := 0; x < w; x++ {
+			for y := 0; y < h; y++ {
+				palettedImg.Set(x, y, rgbaImg.At(x, y))
+			}
+		}
+		images = append(images, palettedImg)
 	}
 
-	f, _ := os.OpenFile("sine.gif", os.O_WRONLY|os.O_CREATE, 0600)
+	f, err := os.OpenFile("sine.gif", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 	defer f.Close()
-	gif.EncodeAll(f, &gif.GIF{
+
+	err = gif.EncodeAll(f, &gif.GIF{
 		Image: images,
 		Delay: delays,
 	})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
 }
